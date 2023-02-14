47046cdf0a7ee84c25a05ea0db1de7ca3bd6c109
Daniels Gabranovs <86777283+Lapegle@users.noreply.github.com>
2023-02-14 12:00:59 +0200

    a

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,7 +12,7 @@ try {
  */
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = 'http://localhost:8000/'
+window.axios.defaults.baseURL = 'http://localhost/'
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.withCredentials = true
